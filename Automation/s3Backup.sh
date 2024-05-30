49216e295529e4ca8d63c36ad0a8bf239b3f3e0f
Shivam Gupta <shivam17.gupta@paytm.com>
2024-05-30 14:17:33 +0530

    qoala assignment

--- Automation/s3Backup.sh
+++ Automation/s3Backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 # Variables
 SOURCE_BUCKET="your-source-s3-bucket-name"
 DEST_BUCKET="your-destination-s3-bucket-name"
@@ -13,3 +15,11 @@ if [ $? -eq 0 ]; then
 else
   echo "Backup of S3 bucket $SOURCE_BUCKET to $DEST_BUCKET failed."
 fi
+
+
+ 
+## Two ways to use this script that i can think of 
+#1. create a jenkins job that runs everyday at a specific time   .. that will create backup of s3 bucket
+#2. Create a cronjob in a server on in kubernetes that will run at a specific time and will backup the s3 bucket
+
+
